cee54fd2e4ab6c0228424e3576bedb1e0c91e8e8
shaikrasheed <shaikrasheed6341@gmail.com>
2025-04-09 22:44:58 +0530

    env setup

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,33 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
+
 import ownerRoutes from "./src/routes/ownerRoutes.js";
-import custmorRoutes from "./src/routes/custmorRoutes.js";
+import customerRoutes from "./src/routes/custmorRoutes.js";
 import itemsRoutes from "./src/routes/itemsRoutes.js";
-import qutation from "./src/routes/qutation.js";
-import bankdetails from "./src/routes/bankdetails.js";
-// import registerRoute from "./src/routes/registerRoute.js";
-// import loginRoute from "./src/routes/loginRoute.js";
-// import authmiddle from "./src/routes/authmiddleware.js";
+import quotationRoutes from "./src/routes/qutation.js";
+import bankDetailsRoutes from "./src/routes/bankdetails.js";
+
+dotenv.config();
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://invoice-genrator-tvpk-git-main-shaikrasheeds-projects.vercel.app", // New frontend URL
+    origin: process.env.VITE_BAKEND_URL,
     methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
-    optionsSuccessStatus: 200, // Handle preflight
 }));
 
-// Manual header fallback
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://invoice-genrator-tvpk-git-main-shaikrasheeds-projects.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
-
-app.use("/quation", qutation);
-app.use("/custmor", custmorRoutes);
+app.use("/quotation", quotationRoutes);
+app.use("/customer", customerRoutes);
 app.use("/owners", ownerRoutes);
-app.use("/iteam", itemsRoutes);
-app.use("/bank", bankdetails);
+app.use("/items", itemsRoutes);
+app.use("/bank", bankDetailsRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
 export default app;
